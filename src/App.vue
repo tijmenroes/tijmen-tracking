fb66d96bf2db98b9b99cb18b32ec165f0766a03b
Tijmen Roes <roes.tijmen@gmail.com>
2026-05-18 21:30:41 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<script setup lang="ts">
+console.log(import.meta.env.VITE_SUPABASE_URL)
+console.log(import.meta.env.VITE_SUPABASE_ANON_KEY)
+</script>
+
+<style scoped></style>
